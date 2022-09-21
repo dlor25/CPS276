7a30a24a5baa21b323b7dda82897383c21ea00db
dlor25 <77540225+dlor25@users.noreply.github.com>
2022-09-20 21:34:27 -0400

    updates

---
+++ Examples/Testarea/test.php
@@ -0,0 +1,18 @@
+<?php
+$name = "Dan"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, intital-scale=1.0">
+        <title> html elements</title>
+    </head>
+    <body>
+        <h1>Hello world</h1>
+        <p> <?php echo $name; ?></p>    <!--This is a comment for HTML only-->
+    </body>
+</html>
---
+++ Examples/index.php
@@ -0,0 +1,40 @@
+<?php 
+
+
+
+?>
+
+
+
+    <meta name"viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<p>Color:</p>
+<label><input type="radio" name="color" value="blue" > Blue </label>
+<label><input type="radio" name="color" value="red" > Red </label>
+<label><input type="radio" name="color" value="yellow" > Yellow </label>
+
+<p>Size:</p>
+<label><input type="radio" name="size" value="small" > Small </label>
+<label><input type="radio" name="size" value="medium" >  Medium </label>
+<label><input type="radio" name="size" value="large" > Large  </label>
+
+
+
+<p>Sizes:</p>
+<label><input type="checkbox"name="size"value="small"> Small</label>
+<label><input type="checkbox"name="size"value="medium"> Medium</label>
+<label><input type="checkbox"name="size"
