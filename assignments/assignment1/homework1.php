1463949aa9698e5ebb8a41b8ff0754fddb0dc076
dlor25 <77540225+dlor25@users.noreply.github.com>
2022-09-20 23:05:08 -0400

    updates

--- assignments/assignment1/homework1.php
+++ assignments/assignment1/homework1.php
@@ -5,94 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
-    <title>Assignment1</title>
+    <title>Form Project</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
   </head>
   <body class="p-3 m-0 border-0 bd-example">
 
     <!-- Example Code -->
     
-    <form class="row g-3">
-    <div class="col-md-6">
+    <form class="row g-3" method="post" action="#">
+    <div class="container">
       <label for="inputfirstname" class="form-label">First Name</label>
         <input type="name" class="form-control" id="inputfirstname">
       </div>
-      <div class="col-md-6">
+      <div class="container">
         <label for="inputlastname" class="form-label">Last Name</label>
         <input type="name" class="form-control" id="inputlastname">
       </div> 
-      <div class="col-12">
+      <div class="container">
         <label for="inputAddress" class="form-label">Address</label>
         <input type="text" class="form-control" id="inputAddress">
       </div>
-      <div class="col-md-6">
+      <div class="container">
         <label for="inputCity" class="form-label">City</label>
         <input type="text" class="form-control" id="inputCity">
       </div>
-      <div class="col-md-4">
+      <div class="container">
         <label for="inputState" class="form-label">State</label>
         <select id="inputState" class="form-select">
         <option>Alabama</option>
-        <option>Alaska</option>
-        <option>Arizona</option>
-        <option>Arkansas</option>
         <option>California</option>
-        <option>Colorado</option>
-        <option>Connecticut</option>
-        <option>Delaware</option>
-        <option>Florida</option>
-        <option>Georgia</option>
-        <option>Hawaii</option>
-        <option>Idaho</option>
-        <option>Illinois</option>
-        <option>Indiana</option>
-        <option>Iowa</option>
-        <option>Kansas</option>
-        <option>Kentucky</option>
-        <option>Louisiana</option>
-        <option>Maine</option>
-        <option>Maryland</option>
-        <option>Massachusetts</option>
         <option selected>Michigan</option>
-        <option>Minnesota</option>
-        <option>Mississippi</option>
-        <option>Missouri</option>
-        <option>Montana</option>
-        <option>Nebraska</option>
-        <option>Nevada</option>
-        <option>New Hampshire</option>
-        <option>New Jersey</option>
-        <option>New Mexico</option>
         <option>New York</option>
-        <option>North Carolina</option>
-        <option>North Dakota</option>
-        <option>Ohio</option>
-        <option>Oklahoma</option>
-        <option>Oregon</option>
-        <option>Pennsylvania</option>
-        <option>Rhode Island</option>
-        <option>South Carolina</option>
-        <option>South Dakota</option>
-        <option>Tennessee</option>
-        <option>Texas</option>
-        <option>Utah</option>
         <option>Vermont</option>
-        <option>Virginia</option>
-        <option>Washington</option>
-        <option>West Virginia</option>
-        <option>Wisconsin</option>
-        <option>Wyoming</option>
         </select>
       </div>
-      <div class="col-md-2">
+      <div class="container">
         <label for="inputZip" class="form-label">Zip</label>
         <input type="text" class="form-control" id="inputZip">
       </div>
-      <div class="col-12">
+      <div class="container">
         <label><input type="radio" name="gender" value="male" > Male </label>
         <label><input type="radio" name="gender" value="female" > Female </label>
       </div>
-      <div class="col-12">
+      <div class="container">
         <button type="submit" class="btn btn-primary">Register</button>
       </div>
     </form>
--- assignments/assignment1/homework1test.php
+++ assignments/assignment1/homework1test.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
-    <title>Assignment1</title>
+    <title>Form Project</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
   </head>
   <body class="p-3 m-0 border-0 bd-example">
 
     <!-- Example Code -->
     
-    <form class="row g-3">
+    <form class="row g-3" method="post" action="#">
     <div class="col-md-6">
       <label for="inputfirstname" class="form-label">First Name</label>
         <input type="name" class="form-control" id="inputfirstname">
@@ -33,55 +33,10 @@
         <label for="inputState" class="form-label">State</label>
         <select id="inputState" class="form-select">
         <option>Alabama</option>
-        <option>Alaska</option>
-        <option>Arizona</option>
-        <option>Arkansas</option>
         <option>California</option>
-        <option>Colorado</option>
-        <option>Connecticut</option>
-        <option>Delaware</option>
-        <option>Florida</option>
-        <option>Georgia</option>
-        <option>Hawaii</option>
-        <option>Idaho</option>
-        <option>Illinois</option>
-        <option>Indiana</option>
-        <option>Iowa</option>
-        <option>Kansas</option>
-        <option>Kentucky</option>
-        <option>Louisiana</option>
-        <option>Maine</option>
-        <option>Maryland</option>
-        <option>Massachusetts</option>
         <option selected>Michigan</option>
-        <option>Minnesota</option>
-        <option>Mississippi</option>
-        <option>Missouri</option>
-        <option>Montana</option>
-        <option>Nebraska</option>
-        <option>Nevada</option>
-        <option>New Hampshire</option>
-        <option>New Jersey</option>
-        <option>New Mexico</option>
         <option>New York</option>
-        <option>North Carolina</option>
-        <option>North Dakota</option>
-        <option>Ohio</option>
-        <option>Oklahoma</option>
-        <option>Oregon</option>
-        <option>Pennsylvania</option>
-        <option>Rhode Island</option>
-        <option>South Carolina</option>
-        <option>South Dakota</option>
-        <option>Tennessee</option>
-        <option>Texas</option>
-        <option>Utah</option>
         <option>Vermont</option>
-        <option>Virginia</option>
-        <option>Washington</option>
-        <option>West Virginia</option>
-        <option>Wisconsin</option>
-        <option>Wyoming</option>
         </select>
       </div>
       <div class="col-md-2">
