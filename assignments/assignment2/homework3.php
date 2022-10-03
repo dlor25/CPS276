1b56545acb4e1911d7006ac947dfd4f075d3f97a
dlor25 <77540225+dlor25@users.noreply.github.com>
2022-10-03 14:53:03 -0400

    updates

--- assignments/assignment2/homework3.php
+++ assignments/assignment2/homework3.php
@@ -1,13 +1,13 @@
 <?
 $row = 15;
-$column = 5;
+$cell = 5;
 ?>
 
 <table border=1 cellspacing=2 cellpadding=2>
     <? for ($counter=1;$counter<=$row;$counter++) { ?>
         <tr>
-                <? for ($counter1=1;$counter1<=$column;$counter1++) { ?>
-                    <td>Row <? echo($counter) ?> Column <? echo($counter1) ?></td>
+                <? for ($counter1=1;$counter1<=$cell;$counter1++) { ?>
+                    <td>Row <? echo($counter) ?> Cell <? echo($counter1) ?></td>
                         <? } ?>
         </tr>
     <? } ?>
