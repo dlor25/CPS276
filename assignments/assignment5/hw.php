619a997bb4a8b7fbfd62750e089ed291263cb79d
dlor25 <77540225+dlor25@users.noreply.github.com>
2022-10-22 21:44:12 -0400

    updates

--- assignments/assignment5/hw.php
+++ assignments/assignment5/hw.php
@@ -15,43 +15,48 @@ if(count($_POST) > 0){
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>Directory</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" href="public/css/main.css">
-  <style>
-  body {
-    font: 250%/1.5 sans-serif;
-  }
-</style>
-  </head>
-  <body>
-    <div id="wrapper" class="container">
-    <h1>Files and Directory Assignment</h1>
-      <p>Enter folder name and file contents. Folder names should contain alpha-numeric characters only.</p>
-      <p><?php echo $msg; 
-//      echo $output; ?></p>    <!-- Reset button set-up 2/3 --> 
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	    <link rel="stylesheet" href="public/css/main.css">
+        <title>Directory</title>
 
-      <form method="POST">
+        <style>
+        body {
+            font: 250%/1.5 sans-serif;
+        }
+        </style>
 
-      <div class="form-group">
+    </head>
+
+    <body>
+        <div id="wrapper" class="container">
+            <h1>Files and Directory Assignment</h1>
+            <p>Enter folder name and file contents. Folder names should contain alpha-numeric characters only.</p>
+            <p><?php echo $msg; 
+        //      echo $output; ?></p>    <!-- Reset button set-up 2/3 --> 
+
+            <form method="POST">
+
+            <div class="form-group">
                 <label for="foldername">Folder Name</label>
                 <input type="text" class="form-control" name="foldername" id="foldername">
-      </div>
+            </div>
 
-      <div class="form-group">
+            <div class="form-group">
                 <label for="foldercontent">Folder Content</label>
                 <textarea style="height: 154px;" class="form-control" id="foldercontent" name="foldercontent"></textarea>
-      </div>
+            </div>
 
-        <input type="submit" class="btn btn-primary" name="submit" value="Submit">
- <!--       <input type="submit" class="btn btn-primary" name="remove" value="Reset">   Reset button set-up 3/3    -->
-      </form>
-      
-    </div>
-    
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" name="submit" value="Submit">
+        <!--       <input type="submit" class="btn btn-primary" name="remove" value="Reset">   Reset button set-up 3/3    -->
+            </div>
 
-  </body>
+            </form>
+        
+        </div>
+        
+    </body>
 </html>
